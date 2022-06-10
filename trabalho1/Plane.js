af8dee8307f0e5d33125d5eb2e751d3a5af14fb9
Rômulo Soares <romulosoares@ice.ufjf.br>
2022-06-10 14:24:16 -0300

    Update plane class

--- trabalho1/Plane.js
+++ trabalho1/Plane.js
@@ -12,25 +12,12 @@ export default class Plane {
       this.mesh = new THREE.Mesh(this.#geometry, this.#material);
       this.mesh.position.set(0,6,0);
       this.mesh.rotateX(-1.6);
-      // this.boundingBox.copy(this.mesh.geometry.boundingBox).applyMatrix4(this.mesh.matrixWorld);
+      this.mesh.geometry.computeBoundingBox();
+      this.boundingBox.copy(this.mesh.geometry.boundingBox).applyMatrix4(this.mesh.matrixWorld);
    }
 
-   get mesh() {
-      return this.mesh;
-   }
-
-   get boundingBox() {
+   getBoundingBox() {
       return this.boundingBox;
    }
 
-
 }
-
-//Criando o avião
-// const geometry = new THREE.ConeGeometry( 5, 15, 64 );
-// const material = new THREE.MeshLambertMaterial( {color:'rgb(180,180,255)'} );
-// var cone = new THREE.Mesh( geometry, material );
-// cone.position.set(0,6,0);
-// cone.rotateX(-1.6);
-
-// cone.geometry.computeBoundingBox();
